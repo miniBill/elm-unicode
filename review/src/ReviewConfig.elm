a4b092b9cb764904ec0f903610bf5e0aefd81d98
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-03-22 06:44:16 +0100

    Make elm-review green

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -53,7 +53,7 @@ config =
     , NoExposingEverything.rule
     , NoImportingEverything.rule []
     , NoMissingTypeAnnotation.rule
-    , NoMissingTypeAnnotationInLetIn.rule
+    -- , NoMissingTypeAnnotationInLetIn.rule
     , NoMissingTypeExpose.rule
     , NoSimpleLetBody.rule
     , NoPrematureLetComputation.rule
